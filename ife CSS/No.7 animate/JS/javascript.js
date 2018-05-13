801e64da3792f8a8988fac8314e64c0bfcf03508
NN <nwt_NN@163.com>
2018-05-13 16:55:51 +0800

    No.7 - 使用 animate.css 实现一个优雅的登录框

--- ife CSS/No.7 animate/JS/javascript.js
+++ ife CSS/No.7 animate/JS/javascript.js
@@ -1,6 +1,6 @@
 $(function () {
   $('#email').focus(function () { // 获取焦点
-    $('label[for="email"] span').addClass('active') //加蓝条
+    $('label[for="email"] span').addClass('active') //文字上移
     $('#email~div').addClass('bar')
   }).blur(function () { //失去焦点
      $('#email~div').addClass('bar_animate') //蓝条消失
@@ -10,7 +10,7 @@ $(function () {
     }, 1000);
   })
   $('#password').focus(function () { // 获取焦点
-    $('label[for="password"] span').addClass('active') //加蓝条
+    $('label[for="password"] span').addClass('active') //文字上移
 
     $('#password~div').addClass('bar')
   }).blur(function () { //失去焦点
